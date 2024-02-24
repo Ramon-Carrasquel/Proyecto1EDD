5ad78844b755841cfa51ed4699f81c0dee409297
Sebas4624 <h.sebastian@correo.unimet.edu.ve>
2024-02-24 04:14:32 -0400

    'PrototipoProyecto(NoInterface)'

---
+++ Proyecto_1_EdD/src/proyecto_1_edd/Nodo.java
@@ -0,0 +1,20 @@
+
+package proyecto_1_edd;
+
+
+
+public class Nodo {
+    
+    int value;
+    Nodo next;
+    
+    Nodo(int value) {
+        this.value = value;
+    }
+    
+    Nodo(int value, Nodo next) {
+        this.value = value;
+        this.next = next;
+    }
+    
+}

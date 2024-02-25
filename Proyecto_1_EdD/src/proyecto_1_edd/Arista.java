d1162c8caf1b2eff6faddaed9f6355cdd8d361e1
sebasalf <leon.sebastian@correo.unimet.edu.ve>
2024-02-25 13:46:04 -0400

    'versionalpha'

---
+++ Proyecto_1_EdD/src/proyecto_1_edd/Arista.java
@@ -0,0 +1,67 @@
+
+package proyecto_1_edd;
+
+
+
+public class Arista {
+    
+    double valor;
+    double feromona;
+    int src;
+    int dst;
+    Arista next;
+    
+    
+    Arista(int src_, int dst_, double value) {
+        int ciudad1 = src_;
+        int ciudad2 = dst_;
+        
+        valor = value;
+        feromona = 1;
+        if(ciudad1 < 0) {
+            ciudad1 *= -1;
+        }
+        if(ciudad2 < 0) {
+            ciudad2 *= -1;
+        }
+                
+        if(ciudad1 > ciudad2) {
+            src = ciudad2;
+            dst = ciudad1;
+        }
+        else {
+            src = ciudad1;
+            dst = ciudad2;
+        }
+    }
+    
+    Arista(int src_, int dst_, double value, Arista next) {
+        int ciudad1 = src_;
+        int ciudad2 = dst_;
+        
+        valor = value;
+        feromona = 1;
+        if(ciudad1 > ciudad2) {
+            src = ciudad2;
+            dst = ciudad1;
+        }
+        else {
+            src = ciudad1;
+            dst = ciudad2;
+        }
+        this.next = next;
+    }
+    
+    public void cantFeroInic(int cantCiudades) {
+        this.feromona = feromona / (double) cantCiudades;
+    }
+    
+    public void setFero(double valor) {
+        this.feromona = valor;
+    }
+    
+    public void printFeromona() {
+        System.out.println(feromona);
+    }
+    
+}
--- Proyecto_1_EdD/src/proyecto_1_edd/Grafo.java
+++ Proyecto_1_EdD/src/proyecto_1_edd/Grafo.java
@@ -1,30 +1,73 @@
 
 package proyecto_1_edd;
 
-
+/**
+ * Esta clase define la creación y el manejo del contenido de los grafos
+ * @author sebas
+ * @version 18/02/2024/A
+ */
 
 public class Grafo {
     
-    //private Lista aristas;
+    //Campos de la clase
+    int ciudadOrigen;
+    int ciudadDestino;
     private int maxNodos;
-    private int[][] matriz = new int[20][20];
+    private double[][] matriz = new double[20][20];
     
-    Grafo(int n) {
+    /**
+     * Constructor para el grafo
+       * @param n Define el tamaño que va a poseer el grafo
+       * @param cOrg Define la ciudad de origen para el recorrido
+       * @param cDes Define la ciudad de destino para el recorrido
+     */
+    
+    Grafo(int n, int cOrg, int cDes) {
+        
         int y = n;
+        
         if(y < 4) {
             y = 4;
         }
-        this.maxNodos = y;
-        //aristas = new Lista("aristas_");
+        else if(y > 20) {
+            y = 20;
+        }
         
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
+        this.maxNodos = y;
+        
+        
+        if(cOrg < 1 || cOrg > maxNodos) {
+            cOrg = 1;
+        }
+        else {
+            ciudadOrigen = cOrg;
+        }
+        if(cDes < 1 || cDes > maxNodos) {
+            cDes = y;
+        }
+        else {
+            ciudadDestino = cDes;
+        }
+        if(cOrg == cDes) {
+            ciudadOrigen = 1;
+            ciudadDestino = maxNodos;
+        }
+        
+        
+        for (int i = 0; i < y; ++i) {
+            for (int j = 0; j < y; ++j) {
                 matriz[i][j] = 0;
             }
         }
-    }
+        
+    }//Cierre del constructor
     
     public void añadirCiudad() {
+        
+        /**
+         * Método que añade una ciudad al grafo existente
+         */
+        
         if(maxNodos < 20) {
             maxNodos++;
 
@@ -33,89 +76,161 @@ public class Grafo {
                 matriz[maxNodos - 1][i] = 0;
             }
         }
-    }
+    }//Cierre del método
     
-    public void añadirArista(int src, int dst, int dist) {
+    /**
+     * Método que añade una arista al grafo existente
+     * @param src Establece la ciudad "1" de la arista a crear
+     * @param dst Establece la ciudad "2" de la arista a crear
+     * @param valor Define el tamaño de la arista o distancia entre las dos ciudades
+     */
+    
+    public void añadirArista(int src, int dst, double valor) {
+        
         if(src != dst) {
-            matriz[src][dst] = dist;
-            matriz[dst][src] = dist;
-            //int arista = dist;
-            //aristas.InsertLast(arista);
+            matriz[src][dst] = valor;
+            matriz[dst][src] = valor;
         }
-    }
+    }//Cierre del método
+    
+    /**
+     * Método que elimina una ciudad del grafo existente
+     * @param x Define la ciudad a ser eliminada (Ej.: Si se desea eliminar la ciudad 1, se inserta "1")
+     */
     
     public void eliminarCiudad(int x) {
+        
         if(x > maxNodos) {
             
         }
         else {
             if(maxNodos > 4) {
                 while(x < maxNodos) {
-
+                    
                     for(int i = 0; i < maxNodos; i++) {
                         matriz[i][x] = matriz[i][x + 1];
                     }
-
+                    
                     for(int i = 0; i < maxNodos; i++) {
                         matriz[x][i] = matriz[x + 1][i];
                     }
-
+                    
                     x++;
                 }
-               
+                
                 maxNodos--;
             }
         }
-    }
+    }//Cierre del método
+    
+    /**
+     * Método que elimina una arista del grafo existente
+     * @param src Establece la ciudad "1" de la arista a eliminar
+     * @param dst Establece la ciudad "2" de la arista a eliminar
+     */
     
     public void eliminarArista(int src, int dst) {
+        
         if(src != dst) {
-            //int inX = aristas.SearchValue(matriz[src][dst]);
             matriz[src][dst] = 0;
             matriz[dst][src] = 0;
-            //aristas.Delete(inX);
         }
-    }
+    }//Cierre del método
     
-    public void establCiudadPartida(int c) {
+    public void borrarAristas() {
         
-    }
-    
-    public void establCiudadLlegada(int c) {
+        /**
+         * Método que elimina todas las aristas del grafo existente
+         */
         
-    }
-    
-    public void dFS(int src) {
-        boolean[] visitado = new boolean[matriz.length];
-        dFSH(src, visitado);
-    }
-    
-    public void dFSH(int src, boolean[] visitado) {
-        if(visitado[src]) {
-            return;
-        }
-        else {
-            visitado[src] = true;
-            System.out.println(src + " = Visitado");
-        }
-        for(int i = 0; i < matriz[src].length; i++) {
-            if(matriz[src][i] != 0) {
-                dFSH(i, visitado);
+            for(int i = 0; i < maxNodos; i++) {
+                for(int j = 0; j < maxNodos; j++) {
+                    matriz[i][j] = 0;
+                }
             }
-        }
-        return;
-    }
+        }//Cierre del método
     
     //---------------------------------//
     
+    /**
+     * Método que devuelve el tamaño horizontal de una fila (ciudad) en concreto (útil para encontrar las aristas de ésta)
+     * @param src Define el índice de la fila en cuestión (Ej.: Si se desea la fila 2, se inserta "1")
+     * @return El tamaño de una fila concreta
+     */
+    
+    public int getTamañoHorizontal(int src) {
+        return matriz[src].length;
+    }//Cierre del método
+    
+    /**
+     * Método que devuelve el valor (tamaño) de una arista en concreto
+     * @param src Establece la ciudad "1" conectada a la arista
+     * @param dst Establece la ciudad "2" conectada a la arista
+     * @return El valor (tamaño) de una arista expecífica
+     */
+    
+    public double getValorArista(int src, int dst) {
+        return matriz[src][dst];
+    }//Cierre del método
+    
+    /**
+     * Método que devuelve la cantidad de aristas adheridas a una ciudad en específico
+     * @param src Define el índice de la fila en la cual buscar las aristas (Ej.: Si se desea buscar aristas en la fila 2, se inserta "1")
+     * @return El número de aristas conectadas a una ciudad en específico
+     */
+    
+    public int getCantAristasAdy(int src) {
+        int cantidadA = 0;
+        for(int i = 0; i < matriz[src].length; i++) {
+            if(matriz[src][i] != 0) {
+                cantidadA += 1;
+            }
+        }
+        return cantidadA;
+    }//Cierre del método
+    
+    /**
+     * Método que devuelve el tamaño del grafo
+     * @return El tamaño del grafo
+     */
+    
+    public int tamaño() {
+        return maxNodos;
+    }//Cierre del método
+    
+    /**
+     * Método que devuelve la ciudad origen donde empieza el recorrido
+     * @return La ciudad de origen
+     */
+    
+    public int getCiudadOrigen() {
+        return ciudadOrigen;
+    }//Cierre del método
+    
+    /**
+     * Método que devuelve la ciudad destino donde termina el recorrido
+     * @return La ciudad de destino
+     */
+    
+    public int getCiudadDestino() {
+        return ciudadDestino;
+    }//Cierre del método
+    
+    //---------------------------------//
+    
+    /**
+     * Método que imprime todo el grafo
+     */
+    
     public void print() {
+        System.out.println();
         for(int i = 0; i < maxNodos; i++) {
             for(int j = 0; j < maxNodos; j++) {
-                System.out.print(matriz[i][j] + " ");
+                System.out.print(matriz[i][j] + "   ");
             }
             System.out.println();
         }
         System.out.println();
-    }
+    }//Cierre del método
     
 }
